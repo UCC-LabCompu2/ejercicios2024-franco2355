6ecccd92448e66e42697658837b72bfcb14b31a0
franco2355 <142048169+franco2355@users.noreply.github.com>
2024-05-13 10:44:50 -0300

    Add files via upload

---
+++ Ejercicios-JavaScript/function cambiar-unidades {.js
@@ -0,0 +1,23 @@
+function cambia_unidades(valor, id) {
+    if (isNaN(valor)) {
+        alert("Se ingresó un valor inválido " + id);
+        document.lasUnidades.unid_metro.value = "";
+        document.lasUnidades.unid_pulgada.value = "";
+        document.lasUnidades.unid_pie.value = "";
+        document.lasUnidades.unid_yarda.value = "";
+    }
+
+    if (id === "metro") {
+        document.lasUnidades.unid_pulgada.value = 39.3701 * valor;
+        document.lasUnidades.unid_pie.value = 3.28084 * valor;
+        document.lasUnidades.unid_yarda.value = 1.09361 * valor;
+    } else if (id === "pulgada") {
+        document.lasUnidades.unid_metro.value = 0.0254 * valor;
+        document.lasUnidades.unid_pie.value = 0.0833333 * valor;
+        document.lasUnidades.unid_yarda.value = 0.0277778 * valor;
+    } else if (id === "yarda") {
+        document.lasUnidades.unid_metro.value = 0.9144 * valor;
+        document.lasUnidades.unid_pulgada.value = 36 * valor;
+        document.lasUnidades.unid_pie.value = 3 * valor;
+    }
+}
