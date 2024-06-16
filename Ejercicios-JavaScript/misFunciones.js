676a14dd03bf0aa80b7f69498370a5bd3cf8cbf4
franco2355 <142048169+franco2355@users.noreply.github.com>
2024-06-16 11:49:53 -0300

    Update misFunciones.js

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -278,4 +278,16 @@ let dibujarCuadriculado = () => {
     }
 };
 
+function dibujarImagen(posX, posY) {
+    var canvas = document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+    console.log(posX, posY);
+    var img = new Image();
+    img.src = "images/auto.png";
+    canvas.width = canvas.width;
+    img.onload = function() {
+        ctx.drawImage(img, posX, posY);
+    }
+}
+
 
